3fd4a04455150db52209cf07a6f90935c93cfb72
eddsticks <ewaldo.jefe@gmail.com>
2025-06-19 10:48:20 -0400

    implementación menú con ambas consolas

--- src/main/java/Launcher/Main.java
+++ src/main/java/Launcher/Main.java
@@ -1,9 +1,47 @@
 package Launcher;
 
 import Vista.ConsolaJuegoIndividual;
+import Vista.ConsolaJuegoVersus;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner mainScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        ConsolaJuegoIndividual.menu();
+        System.out.println("==== Bienvenido al Juego de Dados ====");
+        String opcionPrincipal;
+
+        do {
+            mostrarMenuPrincipal();
+            opcionPrincipal = mainScanner.nextLine();
+            ejecutarOpcionPrincipal(opcionPrincipal);
+        } while (!opcionPrincipal.equals("3"));
+
+        System.out.println("Gracias por jugar! Cerrando la aplicación...");
+        mainScanner.close();
+    }
+
+    private static void mostrarMenuPrincipal() {
+        System.out.println("\n--- Menú Principal ---");
+        System.out.println("1. Jugar Modo Individual");
+        System.out.println("2. Jugar Modo Versus");
+        System.out.println("3. Salir de la aplicación");
+        System.out.print("Seleccione una opción: ");
+    }
+
+    private static void ejecutarOpcionPrincipal(String opcion) {
+        switch (opcion) {
+            case "1":
+                ConsolaJuegoIndividual.menu();
+                break;
+            case "2":
+                ConsolaJuegoVersus.menu();
+                break;
+            case "3":
+                break;
+            default:
+                System.out.println("Opción inválida. Por favor, ingrese 1, 2 o 3.");
+                break;
+        }
     }
 }
