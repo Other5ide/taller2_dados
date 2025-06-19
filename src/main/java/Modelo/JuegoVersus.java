c5bb58322159bfcd74ddf9009f0e7e48cd56645a
eddsticks <ewaldo.jefe@gmail.com>
2025-06-19 09:50:47 -0400

    logica versus v1

--- src/main/java/Modelo/JuegoVersus.java
+++ src/main/java/Modelo/JuegoVersus.java
@@ -17,5 +17,69 @@ public class JuegoVersus {
     public String jugarRonda() {
         System.out.println("\n Turno de " + jugador1.getNombre());
         boolean ganoJ1 = juegoDados.jugar();
+
+        System.out.println("\n--- Turno de " + jugador2.getNombre() + " ---");
+        boolean ganoJ2 = juegoDados.jugar();
+
+        String resultadoRonda;
+        if (ganoJ1 && !ganoJ2) {
+            jugador1.incrementarPuntos();
+            resultadoRonda = jugador1.getNombre() + " gana esta ronda.";
+        } else if (ganoJ2 && !ganoJ1) {
+            jugador2.incrementarPuntos();
+            resultadoRonda = jugador2.getNombre() + " gana esta ronda.";
+        } else {
+            resultadoRonda = "Empate en esta ronda. ¡Nadie suma puntos!";
+        }
+        System.out.println(resultadoRonda);
+        System.out.println("Marcador: " + jugador1.getNombre() + " " + jugador1.getPuntos() + " - " + jugador2.getNombre() + " " + jugador2.getPuntos());
+        return resultadoRonda;
     }
+
+    public boolean juegoTerminado() {
+        if (rondasMaximas == 1) {
+            return jugador1.getPuntos() > 0 || jugador2.getPuntos() > 0;
+        }
+
+        int victoriasNecesarias = (rondasMaximas / 2) + 1;
+        return jugador1.getPuntos() >= victoriasNecesarias || jugador2.getPuntos() >= victoriasNecesarias;
+    }
+
+    public Jugador getGanadorFinal() {
+        if (!juegoTerminado()) {
+            return null;
+        }
+
+        if (rondasMaximas == 1) {
+            if (jugador1.getPuntos() > 0) return jugador1;
+            if (jugador2.getPuntos() > 0) return jugador2;
+            return null;
+        }
+
+        int victoriasNecesarias = (rondasMaximas / 2) + 1;
+        if (jugador1.getPuntos() >= victoriasNecesarias) {
+            return jugador1;
+        } else if (jugador2.getPuntos() >= victoriasNecesarias) {
+            return jugador2;
+        }
+        return null;
+    }
+
+    public void reiniciarMarcador() {
+        jugador1.reiniciarPuntos();
+        jugador2.reiniciarPuntos();
+    }
+
+    public Jugador getJugador1() {
+        return jugador1;
+    }
+
+    public Jugador getJugador2() {
+        return jugador2;
+    }
+
+    public int getRondasMaximas() {
+        return rondasMaximas;
+    }
+
 }
