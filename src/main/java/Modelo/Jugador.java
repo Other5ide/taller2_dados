41295c127739f3392ef933bb3f372253637c104a
eddsticks <ewaldo.jefe@gmail.com>
2025-06-19 10:03:22 -0400

    mod jugador edu v1

--- src/main/java/Modelo/Jugador.java
+++ src/main/java/Modelo/Jugador.java
@@ -3,18 +3,32 @@ package Modelo;
 public class Jugador {
     private String nombre;
     private JuegoDados juego;
+    private int puntos;
 
     public Jugador(String nombre) {
         this.nombre = nombre;
         this.juego = new JuegoDados();
+        this.puntos = 0;
     }
 
     public String getNombre() {
         return nombre;
     }
 
+    public int getPuntos() {
+        return puntos;
+    }
+
     public void tirarDados() {
         System.out.println(nombre +" ha tirado sus dados...");
         juego.jugar();
+    }
+
+    public void incrementarPuntos() {
+        this.puntos++;
+    }
+
+    public void reiniciarPuntos() {
+        this.puntos = 0;
     }
 }
