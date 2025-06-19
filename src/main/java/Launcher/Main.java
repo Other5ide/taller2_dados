556469ef11ef994f698e98eaaa04d83145f6887c
eddsticks <ewaldo.jefe@gmail.com>
2025-06-19 09:21:41 -0400

    launcher

--- src/main/java/Launcher/Main.java
+++ src/main/java/Launcher/Main.java
@@ -1,7 +1,18 @@
 package Launcher;
 
-public class Main {
-    public static void main(String[] args) {
+import Modelo.Calculadora;
+import Modelo.JuegoDeDados;
+import Vista.JuegoDadosConsola;
 
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando juego...");
+
+        Calculadora calculadora = new Calculadora();
+        JuegoDeDados juegoIndividual = new JuegoDeDados(calculadora);
+        JuegoDadosConsola consolaJuego = new JuegoDadosConsola(juegoIndividual);
+
+        consolaJuego.iniciar();
     }
 }
